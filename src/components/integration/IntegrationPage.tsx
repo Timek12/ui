f8e30b7e392c8570784e233e5a6b1dd11f0eff2d
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2025-12-21 09:14:10 +0100

    fix(): Remove unused function

--- src/components/integration/IntegrationPage.tsx
+++ src/components/integration/IntegrationPage.tsx
@@ -1,5 +1,5 @@
 import { Terminal } from "lucide-react";
-import React, { useState } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 
 const IntegrationPage: React.FC = () => {
